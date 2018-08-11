446de604f8e7621d1b35480bae6588227121163d
nglsl <nglsl666@aliyun.com>
2018-08-11 11:34:23 +0800

    feat: 提交第三章读书笔记第一部分的代码

--- chap3/src/main/java/xin/codedream/java8/chap3/LambdaExample.java
+++ chap3/src/main/java/xin/codedream/java8/chap3/LambdaExample.java
@@ -3,6 +3,9 @@ package xin.codedream.java8.chap3;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 
 /**
@@ -48,6 +51,51 @@ public class LambdaExample {
         Predicate<String> nonEmptyStringPredicate = (String s) -> !s.isEmpty();
         List<String> stringList = filter(strings, nonEmptyStringPredicate);
         System.out.println(stringList);
+
+        // 使用Consumer
+        forEach(Arrays.asList("Object", "Not", "Found"), (String str) -> System.out.println(str));
+        forEach(Arrays.asList(1, 2, 3, 4, 5, 6), System.out::println);
+
+        // 使用map
+        List<Integer> map = map(Arrays.asList("lambdas", "in", "action"), (String s) -> s.length());
+        System.out.println(map);
+
+        IntPredicate evenNumbers = (int i) -> i % 2 == 0;
+        System.out.println(evenNumbers.test(1000));
+
+        Predicate<Integer> oddNumbers = (Integer i) -> i % 2 == 1;
+        System.out.println(oddNumbers.test(1000));
+    }
+
+
+    /**
+     * 将输入对象的信息映射到输出
+     *
+     * @param list
+     * @param function
+     * @param <T>
+     * @param <R>
+     * @return
+     */
+    private static <T, R> List<R> map(List<T> list, Function<T, R> function) {
+        List<R> result = new ArrayList<>();
+        for (T s : list) {
+            result.add(function.apply(s));
+        }
+        return result;
+    }
+
+    /**
+     * 循环打印
+     *
+     * @param list
+     * @param consumer
+     * @param <T>
+     */
+    private static <T> void forEach(List<T> list, Consumer<T> consumer) {
+        for (T i : list) {
+            consumer.accept(i);
+        }
     }
 
 
@@ -55,6 +103,14 @@ public class LambdaExample {
         r.run();
     }
 
+    /**
+     * 筛选
+     *
+     * @param list
+     * @param predicate
+     * @param <T>
+     * @return
+     */
     private static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
         List<T> result = new ArrayList<>();
         for (T t : list) {
