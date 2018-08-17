3ebb568c4bfc8ea106c0519b9be1b3aed33b4f00
nglsl <nglsl666@aliyun.com>
2018-08-17 23:32:24 +0800

    feat: 提交第三章笔记的代码

--- chap3/src/main/java/xin/codedream/java8/chap3/LambdaExample.java
+++ chap3/src/main/java/xin/codedream/java8/chap3/LambdaExample.java
@@ -2,11 +2,9 @@ package xin.codedream.java8.chap3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.IntPredicate;
-import java.util.function.Predicate;
+import java.util.function.*;
 
 /**
  * Lambda简单示例
@@ -18,7 +16,18 @@ public class LambdaExample {
     public static void main(String[] args) {
         List<Apple> apples = Arrays.asList(new Apple(150, "red"), new Apple(110, "green"), new Apple(100, "green"),
                 new Apple(160, "red"));
+        demo1(apples);
+        methodReference(apples);
+    }
 
+    private static void methodReference(List<Apple> apples) {
+        // 之前
+        apples.sort((Apple a1, Apple a2) -> a1.getWeight().compareTo(a2.getWeight()));
+        // 之后，方法引用
+        apples.sort(Comparator.comparing(Apple::getWeight));
+    }
+
+    private static void demo1(List<Apple> apples) {
         // 简单示例
         Runnable runnable = () -> System.out.println("Hello CodeDream");
         runnable.run();
@@ -65,6 +74,27 @@ public class LambdaExample {
 
         Predicate<Integer> oddNumbers = (Integer i) -> i % 2 == 1;
         System.out.println(oddNumbers.test(1000));
+
+        Comparator<Apple> c1 = (Apple a1, Apple a2) -> a1.getWeight().compareTo(a2.getWeight());
+        ToIntBiFunction<Apple, Apple> c2 = (Apple a1, Apple a2) -> a1.getWeight().compareTo(a2.getWeight());
+        BiFunction<Apple, Apple, Integer> c3 = (Apple a1, Apple a2) -> a1.getWeight().compareTo(a2.getWeight());
+        ;
+
+        Runnable r = () -> {System.out.println("Tricky example");};
+
+        // 参数a没有显示的指定类型
+        List<Apple> greenApples = filter(apples, a -> "green".equals(a.getColor()));
+
+        // 没有类型推断，显示的指定了类型
+        Comparator<Apple> cApple1 = (Apple a1, Apple a2) -> a1.getWeight().compareTo(a2.getWeight());
+
+        // 有类型推断，没有现实的指定类型
+        Comparator<Apple> cApple2 = (a1, a2) -> a1.getWeight().compareTo(a2.getWeight());
+
+        // 外部变量
+        int portNumber = 6666;
+        Runnable r3 = () -> System.out.println(portNumber);
+        r3.run();
     }
 
 
